a4e855db50e66fda79c6cb8714b86a7fdaa6af02
Fajar K <k16101997@gmail.com>
2021-04-09 00:09:29 +0530

    update actions.py

--- rasa_bot/actions/actions.py
+++ rasa_bot/actions/actions.py
@@ -12,6 +12,34 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from mongoengine import connect
+from mongoengine.errors import NotUniqueError, ValidationError
+
+from constants import MONGO_DB, MONGO_USERNAME, MONGO_HOST
+
+class ActionFindFailingServices(Action):
+
+    def name(self) -> Text:
+        return "action_service_latency"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Connect To DB
+        connect(db=MONGO_DB, username=MONGO_USERNAME, host=MONGO_HOST)       
+        # Create Query
+        failedServices = SpanLatency.objects(response_status="failure")
+        # Return Response
+        responses = []
+        for data in failedServices:
+          responses.append(data.span_name)
+        list(dict.fromkeys(response))
+
+        response = 'Hey .....' # Build Apt Response
+        dispatcher.utter_message(text=response)
+
+        return []
 
 class ActionServiceLatency(Action):
 
@@ -22,6 +50,112 @@ class ActionServiceLatency(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Message from action")
+        # Connect To DB
+        connect(db=MONGO_DB, username=MONGO_USERNAME, host=MONGO_HOST)       
+        # Create Query
+        slowServices = SpanLatency.objects(span_kind="SERVER", duration__gte=220).order_by('-duration')
+        # Return Response
+        responses = []
+        for data in slowServices:
+          responses.append(data.span_name)
+        list(dict.fromkeys(response))
+
+        response = 'Hey .....' # Build Apt Response
+        dispatcher.utter_message(text=response)
+
+        return []
+
+class ActionsDegradingServices(Action):
+
+    def name(self) -> Text:
+        return "action_service_latency"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Connect To DB
+        connect(db=MONGO_DB, username=MONGO_USERNAME, host=MONGO_HOST)       
+        
+        # Create Query
+        currentTimestamp 
+        previousHourTimestamp
+        previousDayTimestamp
+        seviceOneRecent = SpanLatency.objects(start_timestamp__gte=previousHourTimestamp, span_name="service1").average('duration')
+        seviceOnePrevious = SpanLatency.objects(start_timestamp__lt=previousHourTimestamp, start_timestamp__gte=, span_name="service1").average('duration')
+        didServiceOneDegrade = seviceOneRecent < seviceOnePrevious ? true : false 
+        seviceTwoRecent = SpanLatency.objects(start_timestamp__gte=previousHourTimestamp, span_name="service2").average('duration')
+        seviceTwoPrevious = SpanLatency.objects(start_timestamp__lt=previousHourTimestamp, start_timestamp__gte=, span_name="service2").average('duration')
+        didServiceTwoDegrade = seviceTwoRecent < seviceTwoPrevious ? true : false 
+        seviceThreeRecent = SpanLatency.objects(start_timestamp__gte=previousHourTimestamp, span_name="service3").average('duration')
+        seviceThreePrevious = SpanLatency.objects(start_timestamp__lt=previousHourTimestamp, start_timestamp__gte=, span_name="service3").average('duration')
+        didServiceThreeDegrade = seviceThreeRecent < seviceThreePrevious ? true : false 
+        
+        # Return Response
+        response = 'Hey .....' # Build Apt Response
+        dispatcher.utter_message(text=response)
+
+        return []
+
+class ActionServiceLoad(Action):
+
+    def name(self) -> Text:
+        return "action_service_load"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Connect To DB
+        connect(db=MONGO_DB, username=MONGO_USERNAME, host=MONGO_HOST)       
+        # Create Query
+        service = tracket.get_slot('slow_service_name')
+        cpuLoad = CpuLoad.objects(service_name=service).cpu_load
+        memLoad = MemLoad.objects(service_name=service).mem_load_bytes
+
+        response = 'Hey .....' # Build Apt Response
+        dispatcher.utter_message(text=response)
+
+        return []
+
+class ActionFindMaintainer(Action):
+
+    def name(self) -> Text:
+        return "action_service_load"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+  
+        service = tracket.get_slot('slow_service_name')
+        maintainers = {
+         'service1' : {
+            'email' : '',
+            'phone' : ''
+            'manager_email' : ''
+          }
+        }        
+        maintainer = maintainers[service]
+        response = 'Hey .....' # Build Apt Response
+        dispatcher.utter_message(text=response)
+
+        return []
+
+class ActionServiceInfo(Action):
+
+    def name(self) -> Text:
+        return "action_service_load"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Connect To DB
+        connect(db=MONGO_DB, username=MONGO_USERNAME, host=MONGO_HOST)       
+        # Create Query
+        service = tracket.get_slot('slow_service_name')
+
+        response = 'Hey .....' # Build Apt Response
+        dispatcher.utter_message(text=response)
 
         return []
