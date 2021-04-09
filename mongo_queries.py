7ac4eff82abf0eb8a3e9605e8f7cc066571c0626
Deepak <deepak.nair@dataweave.com>
2021-04-09 17:19:18 +0530

    Added more queries

--- mongo_queries.py
+++ mongo_queries.py
@@ -19,7 +19,7 @@ top_three_failing_traces_qry = [
     {"$match": {'span_kind': 'SERVER', 'response_status': 'FAILURE'}},
     {'$sort': {"duration": -1}},
     {"$group": {"_id": "$span_name", "trace_ids": {"$push": {"trace_id": "$trace_id"}}}},
-    {"$project": {"trace_ids": {"$slice":["$trace_ids", 2]}}}
+    {"$project": {"trace_ids": {"$slice": ["$trace_ids", 3]}}}
 ]
 top_three_failing_traces = SpanLatency.objects.aggregate(top_three_failing_traces_qry)
 for result in top_three_failing_traces:
@@ -29,3 +29,38 @@ for result in top_three_failing_traces:
     print(f'Service: {service}\nTrace Ids:\n')
     for trace_id in trace_ids:
         print(trace_id)
+print()
+
+# Services that are slower than x milliseconds
+x = 220  # 220 ms
+slow_services = SpanLatency.objects.aggregate([
+    {"$match": {'span_kind': 'SERVER', 'response_status': 'SUCCESS'}},
+    {"$group": {"_id": "$span_name", "average_latency": {"$avg": "$duration"}}},
+    {"$match": {'average_latency': {'$gt': x * 1000}}},  # Milliseconds to Zipkin's microsecond conversion
+])
+print(f'Services having an average latency lesser than {x} milliseconds.')
+
+for result in slow_services:
+    service = result['_id']
+    average_latency = result['average_latency']
+    print(f'{service} has an avg latency of {average_latency}')
+
+print()
+
+# Traces with the worst latency
+traces_with_worst_latency = SpanLatency.objects.aggregate([
+    {"$match": {'span_kind': 'SERVER', 'response_status': 'SUCCESS'}},
+    {"$group": {"_id": "$trace_id", "latency": {"$sum": "$duration"}}},
+    {"$sort": {'latency': -1}},
+    {"$limit": 3}
+])
+
+print(f'Top three traces with worst latency')
+for result in traces_with_worst_latency:
+    trace_id = result['_id']
+    latency = result['latency']
+    print(f'Trace {trace_id} had a latency of {latency}')
+
+print()
+
+# Services which are degrading in performance
